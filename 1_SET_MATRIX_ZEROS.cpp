a073092f9f883f92fc0404959fbc09ed3b1141cb
Aashish Kumar <akrs56@gmail.com>
2023-06-04 14:13:23 +0530

    submission for challenge

---
+++ 1_SET_MATRIX_ZEROS.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	int n,m;
+	n = matrix.size();
+	m = matrix[0].size();
+	vector<int> x1;
+	vector<int> y1;
+	for(int i=0 ; i<n ; i++){
+		for(int j =0 ; j < m ; j++){
+			if(matrix[i][j]==0){
+				x1.push_back(i);
+				y1.push_back(j);
+			}
+		}
+	}
+	for(auto x: x1){
+		for(int j=0; j<m ; j++){
+			matrix[x][j]=0;
+		}
+	}
+	for(auto y: y1){
+		for(int i = 0 ; i< n ; i++){
+			matrix[i][y]=0;
+		}
+	}
+}
