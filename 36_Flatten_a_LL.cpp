0320331e3bf3bd5a4ca54762382a5da89891f099
Aashish Kumar <akrs56@gmail.com>
2023-06-23 21:42:53 +0530

    Improved q36, Now passes all the test cases

--- 36_Flatten_a_LL.cpp
+++ 36_Flatten_a_LL.cpp
@@ -11,66 +11,95 @@
  * };
  */
 
-Node* mergeTwo(Node* head1, Node* head2) {
-    Node* ans = nullptr;
-    Node* head = nullptr;
-    Node* prev = nullptr;
-
-    while (head1 && head2) {
-        if (head1->data < head2->data) {
-            if (!ans) {
-                ans = head1;
-                head = ans;
-            } else {
-                prev->child = head1;
-            }
-            prev = head1;
-            head1 = head1->child;
+Node *mergeL(Node *h1, Node *h2){   
+    Node *ans = new Node();
+    Node *h = ans;
+    while(h1 && h2){
+        if(h1->data < h2->data){
+            ans->child= h1;
+            ans =ans->child;
+            h1= h1->child;
         } else {
-            if (!ans) {
-                ans = head2;
-                head = ans;
-            } else {
-                prev->child = head2;
-            }
-            prev = head2;
-            head2 = head2->child;
+          ans->child = h2;
+          ans = ans->child;
+          h2 = h2->child;
         }
     }
-
-    if (head1) {
-        if (!ans) {
-            ans = head1;
-            head = ans;
-        } else {
-            prev->child = head1;
-        }
+    if(h1){
+        ans->child = h1;
     }
+    else    ans->child=h2; 
 
-    if (head2) {
-        if (!ans) {
-            ans = head2;
-            head = ans;
-        } else {
-            prev->child = head2;
-        }
-    }
-
-    return head;
+    return h->child;
 }
 
 
 Node* flattenLinkedList(Node* head) 
 {
-	if(!head || !head->next)
-		return head;
+	if(!head->next)
+        return head;
 
-	// ek baar mai 2-2 merge krte chalo and ho jayega 
-	Node *ans= head;
-	while(head->next){
-		ans=mergeTwo(ans, head->next);
-		head = head->next;
-	}
-	ans->next=nullptr;
-	return ans;	
+    Node *ch=head;
+    Node *right=head->next;
+    
+    right=flattenLinkedList(right);
+    ch->next=nullptr;
+    ch = mergeL(ch, right);
+    return ch;
 }
+// the Above solution takes O(n*n*m) 
+
+// make another approach using priority queue;
+
+/*
+/*
+ * Definition for linked list.
+ * class Node {
+ *  public:
+ *		int data;
+ *		Node *next;
+ * 		Node *child;
+ *		Node() : data(0), next(nullptr), child(nullptr){};
+ *		Node(int x) : data(x), next(nullptr), child(nullptr) {}
+ *		Node(int x, Node *next, Node *child) : data(x), next(next), child(child) {}
+ * };
+ */
+#include<bits/stdc++.h>
+
+struct mycomp {
+    bool operator()(Node* a, Node* b)
+    {
+        return a->data > b->data;
+    }
+};
+// bool mycomp(Node *a, Node *b){
+//     return a->data < b->data;
+// }
+
+/*
+Node* flattenLinkedList(Node* head) 
+{
+    priority_queue<Node*, vector<Node*>, mycomp> p1;
+    Node *hh= head;
+    while (head) {
+        Node *temp= head->next;
+        head->next=nullptr;
+        p1.push(head);
+        head= temp;
+    }
+    
+    Node *ans= new Node();
+    head=ans;
+    while(!p1.empty()){
+        Node *temp=p1.top();
+        p1.pop();
+        if(temp->child)
+            p1.push(temp->child);
+        ans->child=temp;
+        ans=ans->child;
+    }
+    
+    return head->child;
+}
+
+*/
