fbc3bad89cc2a129b43fd6ab009a915bb366b66a
Aashish Kumar <akrs56@gmail.com>
2023-06-25 10:06:13 +0530

    used moore's algo, more optimised now

--- 15_Majority_ele.cpp
+++ 15_Majority_ele.cpp
@@ -1,19 +1,49 @@
+//#include <bits/stdc++.h>
+
+// int findMajorityElement(int arr[], int n) {
+// 	unordered_map<int, int> m1; 
+// 	int max_=-1;
+// 	int max_el=INT_MAX;
+// 	for(int i=0; i<n; i++){
+// 		m1[arr[i]]++;
+// 		if(m1[arr[i]]>max_){
+// 			max_= m1[arr[i]];
+// 			max_el=arr[i];
+// 		}
+// 	}
+// 	if(max_ > n/2)
+// 		return max_el;
+// 	else
+// 		return -1;
+	
+// }
+
+// 
+
+// optimised soln 
 #include <bits/stdc++.h>
 
 int findMajorityElement(int arr[], int n) {
-	unordered_map<int, int> m1; 
-	int max_=-1;
-	int max_el=INT_MAX;
-	for(int i=0; i<n; i++){
-		m1[arr[i]]++;
-		if(m1[arr[i]]>max_){
-			max_= m1[arr[i]];
-			max_el=arr[i];
+	int cnt=0; 
+	int el; 
+
+	for(int i = 0; i < n ; i++){
+		if(cnt==0){
+			el=arr[i];
 		}
+        if (el == arr[i]) {
+                  cnt++;
+                } 
+		else
+                  cnt--;
+        }
+    cnt=0 ;
+	for(int i =0; i < n ; i++){
+		if(el == arr[i])
+			cnt++;
 	}
-	if(max_ > n/2)
-		return max_el;
-	else
-		return -1;
-	
+	if(cnt > n/2)
+		return el;
+	else return -1;
+
 }
